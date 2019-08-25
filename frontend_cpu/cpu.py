15189b37499870485f5470cd06374d209f9601cc
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-08-25 03:16:37 -0600

    Add files via upload

--- frontend_cpu/cpu.py
+++ frontend_cpu/cpu.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.add_url_rule('/static/<path:filename>', endpoint='static', view_func=app.send_static_file)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
